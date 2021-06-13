0c9ab90ab9ef0fbb1756a50791a6ab38e60e0b05
William Munoz <william.munozr@gmail.com>
2021-06-13 12:40:52 -0500

    GitHub Configuration

--- external_services.tf
+++ external_services.tf
@@ -10,6 +10,7 @@ resource "kubernetes_service" "rds" {
 
   spec {
     type = "ExternalName"
-    external_name = element(split(":", module.db.this_db_instance_endpoint), 0)
+    #external_name = element(split(":", module.db.this_db_instance_endpoint), 0)
+    external_name = module.db.db_instance_endpoint
   }
 }
